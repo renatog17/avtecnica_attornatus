a8149bd109feba59e9815cc5370e06adc5a9bff4
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-01-10 09:01:41 -0300

    setup do projeto

---
+++ src/main/java/br/com/attornatus/avtecnica/AvtecnicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.avtecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvtecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvtecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/avtecnica/AvtecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.avtecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvtecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
